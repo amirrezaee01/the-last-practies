1c8342ffc8bce1d1872a98d3053abe779c36c9ad
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-11-27 13:12:09 +0330

    make the blog model prettier

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from blog.models import Post
 
 
-admin.site.register(Post)
+class PostaAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ['title' ,'counted_views','status','published_date','created_date']
+    list_filter = ['status']
+    ordering = ['-created_date']
+    search_fields = ['title','content']
+    
+admin.site.register(Post,PostaAdmin)
 
