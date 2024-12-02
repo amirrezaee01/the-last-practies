08ee6836363ab612648dfe7ee02b60562e5fea11
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-12-02 21:10:37 +0330

    added image field to model

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    #img
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
