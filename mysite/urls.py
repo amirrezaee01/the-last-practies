e8391289a95408a722f193dc44455e8397e6a192
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-12-08 12:21:13 +0330

    addded sitemaps to project

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,10 +19,25 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from website.views import *
+from website.sitemaps import *
+from blog.sitemaps import *
+from django.contrib.sitemaps.views import sitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    'blog':BlogSitemap
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
     path('blog/',include('blog.urls')),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
